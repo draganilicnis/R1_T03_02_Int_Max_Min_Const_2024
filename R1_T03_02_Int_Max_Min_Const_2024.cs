9a565150f4f32109a72233581193008d8b08ba77
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2024-11-11 13:14:02 +0100

    Update R1_T03_02_Int_Max_Min_Const_2024.cs

--- R1_T03_02_Int_Max_Min_Const_2024.cs
+++ R1_T03_02_Int_Max_Min_Const_2024.cs
@@ -14,6 +14,8 @@ class R1_T03_02_Int_Max_Min_Const_2024
     {
         Int_00_Const_Ispis();
         Int_01_Max();
+        Int_02_HEX_BIN_Raad();
+        Real_03_Const();
     }
     static void Int_00_Const_Ispis()
     {
@@ -45,5 +47,58 @@ class R1_T03_02_Int_Max_Min_Const_2024
         Console.WriteLine("10. float:      " + float.MaxValue);
         Console.WriteLine("11. double:     " + double.MaxValue);
         Console.WriteLine("12. decimal:    " + decimal.MaxValue);
+        Console.WriteLine("13. Half:       " + Half.MaxValue);
+    }
+
+    static void Int_02_HEX_BIN_Raad()
+    {
+        // https://petlja.org/sr-Latn-RS/biblioteka/r/Zbirka/binarni_i_heksadekadni_zapis_broja
+        // Procitaj heksadekadni, ispisi dekadni
+        string sh = "1F";
+        ulong h = Convert.ToUInt64(sh, 16); // ulong h = Convert.ToUInt64(Console.ReadLine(), 16);
+        Console.WriteLine(h);
+
+        // Procitaj binarni, ispisi dekadni
+        string sb = "01000001";
+        ulong b = Convert.ToUInt64(sb, 2); // ulong b = Convert.ToUInt64(Console.ReadLine(), 2);
+        Console.WriteLine(b);
+    }
+    static void Real_03_Const()
+    {
+        // https://petlja.org/sr-Latn-RS/kurs/14606/4/1314
+        double x = 3;
+        x = 3;
+        x = 3.0;
+        x = +3;
+        x = +3.0;
+        x = 3e0;
+        x = 3.0e0;
+        x = +3e0;
+        x = +3.0e0;
+        x = 3E0;
+        x = 3.0E0;
+        x = +3E0;
+        x = +3.0E0;
+        x = 0.3e1;
+        x = .3e1;
+        x = +0.3e1;
+        x = +.3e1;
+        x = 0.03e2;
+        // https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings#BFormatString
+        x = 3.49999999;
+        int b = (int)x;
+        Console.WriteLine(x);
+        Console.WriteLine(x.ToString("0.00"));
+        Console.WriteLine(x.ToString("F2"));
+        Console.WriteLine(x.ToString("E"));
+        Console.WriteLine(x.ToString("e4"));
+        Console.WriteLine(x.ToString("E8"));
+        Console.WriteLine(x.ToString("C3"));
+        Console.WriteLine(b.ToString("D"));
+        Console.WriteLine(x.ToString("F3"));
+        Console.WriteLine(x.ToString("G3"));
+        Console.WriteLine(x.ToString("N1"));
+        Console.WriteLine(x.ToString("P2"));
+        Console.WriteLine(b.ToString("X"));
     }
 }
